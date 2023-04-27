83f219b85228f000f104bf08c361c4396525c2e6
Finn Kuhni <finn.kuhni@bbzbl-it.ch>
2023-04-27 11:37:50 +0200

    new awesome features

--- src/main/java/ch/coop/timetracker/project/Project.java
+++ src/main/java/ch/coop/timetracker/project/Project.java
@@ -1,10 +1,10 @@
 package ch.coop.timetracker.project;
 
-import ch.coop.timetracker.employee.Employee;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
-
-import java.time.LocalDateTime;
 
 @Data
 @Entity
